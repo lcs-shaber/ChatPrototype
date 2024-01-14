f2bc017a9a345cf3f6f9132553aed73de5c233e7
Sophie Haber <SHaber@lcs.on.ca>
2024-01-14 08:53:07 -0500

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Sophie Haber on 14.01.24.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
